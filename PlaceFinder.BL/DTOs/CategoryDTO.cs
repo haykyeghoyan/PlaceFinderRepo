5c79d2bdd8ef3f593073c5d14a5254eb6b3f76c7
yeghoyanhayk96 <yeghoyanhayk96@gmail.com>
2019-06-18 23:18:06 +0400

    last

--- PlaceFinder.BL/DTOs/CategoryDTO.cs
+++ PlaceFinder.BL/DTOs/CategoryDTO.cs
@@ -9,7 +9,6 @@ namespace PlaceFinder.BL.DTOs
         public int Id { get; set; }
         public string Name { get; set; }
         public int ParentId { get; set; }
-
-       
+              
     }
 }
